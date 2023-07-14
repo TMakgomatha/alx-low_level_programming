df5f5362b3d7bb3fae71bf468e532a65e0ea004f
TMakgomatha <tpsebabatso@gmail.com>
2023-07-13 20:45:26 -0700

    Sizes task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* declare main method
+*
+* Return value must always be Zero
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
