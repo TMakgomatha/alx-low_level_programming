14323ae03d4d64a065b8219a5ce4253ce84b1930
TMakgomatha <tpsebabatso@gmail.com>
2023-07-16 01:16:33 -0700

    printing a lphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if(alphabet != 'q' && alphabet != 'e')
+	{
+	         putchar(alphabet);
+		 putchar(' ');
+	}
+	}
+}
