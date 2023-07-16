cb2532d9abe0845f769b648df496134558d53631
TMakgomatha <tpsebabatso@gmail.com>
2023-07-16 02:02:30 -0700

    prining alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	char alphabet;
 
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		if(alphabet != 'q' && alphabet != 'e')
-	        {
-         	putchar(alphabet);
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
 		}
 	}
 	putchar('\n');
