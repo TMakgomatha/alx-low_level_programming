d5c2d18d40ecdb052f6404a8a910e54c5cb0285a
TMakgomatha <tpsebabatso@gmail.com>
2023-07-17 03:05:12 -0700

    printing numbers rectified

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 /**
- * Main - Program prints all single digit from 0 to 9
+ *main -print base numbers 0 to 9
  *
- * Return value is Zer0
+ * Return value must always be Zero
  */
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
-printf("%d\n", i);
-}
-return (0);
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
