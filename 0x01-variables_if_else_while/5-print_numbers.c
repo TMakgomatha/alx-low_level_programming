1263609e793413a64c392bd136dc12384b7faf07
TMakgomatha <tpsebabatso@gmail.com>
2023-07-17 02:33:39 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * Main - Program prints all single digit from 0 to 9
+ *
+ * Return value is Zer0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
