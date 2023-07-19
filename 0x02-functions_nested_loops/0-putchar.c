e27f95ded66d32a8a62993bb55d8603b13b6f3d4
TMakgomatha <tpsebabatso@gmail.com>
2023-07-19 04:45:38 -0700

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *main - print the putchar function followed by a new line
+ *
+ * Return: value must always be Zero
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
